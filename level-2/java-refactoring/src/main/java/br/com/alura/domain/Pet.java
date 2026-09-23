901f159e0eb6eb321881950a8401de7a8eea2f7c
João Abreu <jgabreu2007@gmail.com>
2026-09-23 11:37:51 -0300

    Adjusting the listarPets method

--- level-2/java-refactoring/src/main/java/br/com/alura/domain/Pet.java
+++ level-2/java-refactoring/src/main/java/br/com/alura/domain/Pet.java
@@ -2,6 +2,7 @@ package br.com.alura.domain;
 
 public class Pet {
 
+    private Long id;
     private String tipo;
     private String nome;
     private String raca;
@@ -16,5 +17,25 @@ public class Pet {
         this.idade = idade;
         this.cor = cor;
         this.peso = peso;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public int getIdade() {
+        return idade;
     }
 }
